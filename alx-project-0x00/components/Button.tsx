07ab720c59249ccc388f27d6c9b061d2aa36fa62
BissyClassic <rachaelafolabi2021@gmail.com>
2025-09-18 16:00:13 +0100

    add updated files

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,6 +1,6 @@
  import React from "react";
 
- interface ButtonProps {
+  export interface ButtonProps {
     title: string;
     size?: "small" | "medium" | "large";
     shape?: "rounded-sm" | "rounded-md" | "rounded-full";
