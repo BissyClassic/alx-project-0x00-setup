5a607f8347dbfaad5be75f61e9affb7f633eabe6
BissyClassic <rachaelafolabi2021@gmail.com>
2025-09-18 15:48:51 +0100

    Add updated Button file

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,12 @@
-import Card from "@/components/Card"
-const Landing: React.FC =  () => {
+import Button from "@/components/Button";
+
+export default function LandingPage() {
   return (
-    <div>
-      <h1 className=" text-xl font-extralight">Landing Page</h1>
-      <Card />
+    <div className="flex flex-col gap-4 items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold mb-6">Landing Page</h1>
+      <Button title="Small Rounded-sm" size="small" shape="rounded-sm" />
+      <Button title="Medium Rounded-md" size="medium" shape="rounded-md" />
+      <Button title="Large Rounded-full" size="large" shape="rounded-full" />
     </div>
-  )
+  );
 }
-export default Landing;
